0eda5a18116b5c908ccac965bcb82116d1b8fe12
AnsiyaSharafudeen <ansiyas1997@gmail.com>
2022-06-23 02:53:20 +0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+import Header from './Components/Header/Header';
+
+
+function App() {
+  return (
+    <div className="app">
+      <h1>
+      <Header/>
+
+      </h1>
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Firebase/config.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDYYh-YCsKka-vYW09jL2z8fDdylW7woZ8",
+    authDomain: "clone-79a6d.firebaseapp.com",
+    projectId: "clone-79a6d",
+    storageBucket: "clone-79a6d.appspot.com",
+    messagingSenderId: "219682909875",
+    appId: "1:219682909875:web:f746f99a79056280b80ac6",
+    measurementId: "G-RN3TFMG55H"
+  };
