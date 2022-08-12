0b304cc0f9f3f1c127502ade1a0fc2abf81ee65a
AnsiyaSharafudeen <ansiyas1997@gmail.com>
2022-08-12 21:23:34 +0400

    route added

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Header from "./Components/Header/Header";
 import Home from "./Components/Home/Home";
 
 function App() {
   return (
-    <BrowserRouter>
-      <div className="app">
-        <Routes>
-          <Route path="/">
-            
-            
-          </Route>
-          <Route path="/checkout">
-            <Header />
-            <Home />
-          </Route>
-        </Routes>
+    <Router>
+      <div className="App">
+      <Routes>
+      <Route path="/" element={<><Header/><Home/></>}>
+      </Route>
+      
+      
+    </Routes>
+
       </div>
-    </BrowserRouter>
+   
+    
+    </Router>
+ 
   );
 }
 
